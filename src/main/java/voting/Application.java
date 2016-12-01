5c2492445651df9617cd4fe5605bf12bd70c7511
Li Li <Li.Li@reece.com.au>
2016-12-01 13:39:05 +1100

    remove jdbc

--- src/main/java/voting/Application.java
+++ src/main/java/voting/Application.java
@@ -2,13 +2,11 @@ package voting;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import voting.infrastructure.SQLiteJDBC;
 
 @SpringBootApplication
 public class Application {
 
     public static void main(String[] args) {
-        SQLiteJDBC sqLiteJDBC = new SQLiteJDBC();
         SpringApplication.run(Application.class, args);
     }
 
