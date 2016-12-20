f151e4579d9a3a1fc93f8dba7d0cbde6793fc064
Li Li <Li.Li@reece.com.au>
2016-12-20 16:07:38 +1100

    start with junit test

---
+++ src/test/java/voting/controller/assembler/SurveyTemplateUiModelAssemblerTest.java
@@ -0,0 +1,16 @@
+package voting.controller.assembler;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by lili on 20/12/2016.
+ */
+public class SurveyTemplateUiModelAssemblerTest {
+    @Test
+    public void toSurveyTemplateUiModel() throws Exception {
+
+    }
+
+}
--- src/test/java/voting/infrastructure/JsonSurveyRepositoryTest.java
+++ src/test/java/voting/infrastructure/JsonSurveyRepositoryTest.java
@@ -1,13 +1,20 @@
 package voting.infrastructure;
 
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 import static org.junit.Assert.*;
 
+@RunWith(SpringJUnit4ClassRunner.class)
 public class JsonSurveyRepositoryTest {
-    @Test
-    public void saveSurvey(){
 
+    private String jsonUrl;
+    private String surveyId;
+
+    protected void setUp(){
+        jsonUrl = "test/resources/data.json";
+        surveyId = "67425420-dc3e-435c-bf0f-a785847f5359";
     }
 
     @Test
